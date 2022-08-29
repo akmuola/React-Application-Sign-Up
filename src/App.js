30173551bd0a562d525f15fbb54dd642916e58de
Akinroye Muideen Olawale <akinroyemo@gmail.com>
2022-08-29 09:02:24 +0100

    React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import AddressForm from './components/AddressForm'
+import './Album'
+import Album from './Album';
+import SignUp from './components/SignUp'
+import SignInSide from './components/SignInSide'
+
+function App() {
+  return (
+    <div className="App">
+<SignInSide/>
+    </div>
+  );
+}
+
+export default App;
